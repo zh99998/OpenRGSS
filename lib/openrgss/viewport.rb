1cf271317d8af45248a9db9513a95ef8c204ef7f
zh99998 <zh99998@gmail.com>
2012-09-13 11:45:32 +0800

    viewport

--- lib/openrgss/viewport.rb
+++ lib/openrgss/viewport.rb
@@ -1,11 +1,45 @@
 class Viewport
-  attr_accessor :rect, :visible, :x, :y, :z, :ox, :oy, :color, :tone
+  attr_accessor :rect, :visible, :z, :ox, :oy, :color, :tone
 
   def initialize(*args)
     @tone  = Tone.new
     @color = Color.new
-    @rect  = Rect.new *args
+    args = [0, 0, Graphics.width, Graphics.height] if args.empty?
+    @rect = Rect.new *args
   end
+
+  def x
+    @rect.x
+  end
+
+  def x=(x)
+    @rect.x = x
+  end
+
+  def y
+    @rect.y
+  end
+
+  def y=(y)
+    @rect.y = y
+  end
+
+  def width
+    @rect.width
+  end
+
+  def width=(width)
+    @rect.width = width
+  end
+
+  def height
+    @rect.height
+  end
+
+  def height=(height)
+    @rect.height = height
+  end
+
 
   def dispose
     @disposed = true
