fd87856e04831357340f8cee0779bfdcb5ef95e1
zh99998 <zh99998@gmail.com>
2012-09-14 10:53:16 +0800

    audio

--- lib/openrgss/audio.rb
+++ lib/openrgss/audio.rb
@@ -5,16 +5,15 @@ module Audio
     end
 
     def bgm_play(filename, volume=100, pitch=100, pos=0)
-      SDL::Mixer.set_volume_music(volume)
-      SDL::Mixer.fade_in_music SDL::Mixer::Music.load(RGSS.get_file(filename)), -1, 800
+      SDL::Mixer.play_music SDL::Mixer::Music.load(RGSS.get_file(filename)), -1
     end
 
     def bgm_stop
-
+      SDL::Mixer.halt_music
     end
 
     def bgm_fade(time)
-
+      SDL::Mixer.fade_out_music(time)
     end
 
     def bgm_pos
@@ -22,15 +21,15 @@ module Audio
     end
 
     def bgs_play(filename, volume=100, pitch=100, pos=0)
-
+      @bgs_channel = SDL::Mixer.play_channel(-1, SDL::Mixer::Wave.load(RGSS.get_file(filename)), -1)
     end
 
     def bgs_stop
-
+      SDL::Mixer.halt(@bgs_channel) if @bgs_channel
     end
 
     def bgs_fade(time)
-
+      SDL::Mixer.fade_out(@bgs_channel, time) if @bgs_channel
     end
 
     def bgs_pos
@@ -38,23 +37,23 @@ module Audio
     end
 
     def me_play(filename, volume=100, pitch=100)
-
+      @me_channel = SDL::Mixer.play_channel(-1, SDL::Mixer::Wave.load(RGSS.get_file(filename)), 0)
     end
 
     def me_stop
-
+      SDL::Mixer.halt(@me_channel) if @me_channel
     end
 
     def me_fade(time)
-
+      SDL::Mixer.fade_out(@me_channel, time) if @me_channel
     end
 
     def se_play(filename, volume=100, pitch=100)
-
+      @se_channel = SDL::Mixer.play_channel(-1, SDL::Mixer::Wave.load(RGSS.get_file(filename)), 0)
     end
 
     def se_stop
-
+      SDL::Mixer.halt(@se_channel) if @me_channel
     end
   end
 end
